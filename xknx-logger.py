fed93b455d8f5f8932549b16fd554d0620a46b12
Uwe <usx303@googlemail.com>
2019-10-04 21:33:38 +0200

    logger update

--- xknx-logger.py
+++ xknx-logger.py
@@ -5,7 +5,7 @@ async def telegram_received_cb(telegram):
     print("Telegram received: {0}".format(telegram))
 
 async def main():
-    xknx = XKNX(config="xknx.yaml',telegram_received_cb=telegram_received_cb)
+    xknx = XKNX(config='xknx.yaml', telegram_received_cb=telegram_received_cb)
     await xknx.start(daemon_mode=True)
     await xknx.stop()
 
